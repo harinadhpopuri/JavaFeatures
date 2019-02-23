5081e041eec5c9d52356805841670d7264079209
harinadhpopuri <harinadhpopuri@gmail.com>
2019-02-23 05:35:33 -0500

    Fetures package

---
+++ JavaFetures/src/com/java8/lambda/LambdaExpressions.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.java8.lambda;
+
+/**
+ * @author hpopuri
+ *
+ */
+public class LambdaExpressions {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
